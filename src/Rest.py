af5ba6449edd17f681217a5ebe3bb12e43b9198f
yxl <1132899242@qq.com>
2023-10-10 21:37:42 +0800

    for yxl

--- src/Rest.py
+++ src/Rest.py
@@ -9,7 +9,11 @@ from src.hdbscan import get_merged_polygon_for_hdbscan
 from extract_the_result import load_action_prompt
 from extract_the_result import chat_with_prompt_for_pic
 from extract_the_result import AiCmdEnum
+import logging
 
+# 配置logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 app = FastAPI()
 
@@ -34,13 +38,14 @@ async def upload_file(files: List[UploadFile] = File(...)):
 def get_image_and_ocr_and_result(file):
     image = cv2.imdecode(np.frombuffer(file, np.uint8), cv2.IMREAD_COLOR)
     rectangles = get_ocr(image)
+    logger.info(f'1. OCR ended.')
     characters = get_merged_polygon_for_hdbscan(rectangles)
-
+    logger.info(f'2. Clustering ended.')
     # get prompt from file.
     orc_prompt = load_action_prompt(AiCmdEnum.orc)
-
     result = chat_with_prompt_for_pic(orc_prompt, mode="gpt-3.5-turbo", temperature=0.0, content=characters)
-
+    logger.info(f'3. LLM ended.')
+    logger.info(f'{characters}\n{orc_prompt}\n{result}')
     return result.split('\n')
 
 
--- src/app/main.py
+++ src/app/main.py
@@ -25,10 +25,12 @@ entrypoint of the app
 
 三层架构应用程序的主入口.
 """
-
+import requests
+import uvicorn
 from app.utils.app_exceptions import AppExceptionCase
 from fastapi import FastAPI
 from app.routers import img_outline, reqhistory
+from app.routers.common import img_queue
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from app.utils.request_exceptions import (
@@ -38,8 +40,48 @@ from app.utils.request_exceptions import (
 from app.utils.app_exceptions import app_exception_handler
 import threading
 import app.models.tables as tb
-import logging
 from fastapi.staticfiles import StaticFiles
+from multiprocessing import Process, Queue
+from Rest import get_image_and_ocr_and_result
+import config.constants as ct
+import logging
+import urllib.parse
+
+# 配置logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+
+def img_deal(queue):
+    logger.info(f'OCR process img_deal starting...')
+    while True:
+        item = queue.get()
+        logger.info(f'OCR task confirmed.')
+        pid = item.id
+        img_list = item.img
+
+        i = 0
+        results = {}
+        for file in img_list:
+            i += 1
+
+            img = "picture{}".format(i)
+
+            result = get_image_and_ocr_and_result(file)
+            results[img] = result
+
+        # 用requests上传results
+        encoded_params = urllib.parse.urlencode(results)
+        base_url = "http://127.0.0.1:29081/api/v1/reqhistory/item/?reqid={}&res={}".format(pid, encoded_params)
+
+        response = requests.put(base_url, data=encoded_params)
+
+        if response.status_code == requests.codes.ok:
+            print("PUT请求成功！")
+        else:
+            print("PUT请求失败！")
+        print(results)
+
 
 app = FastAPI(
     title="图片识别与内容提取微服务",
@@ -47,7 +89,7 @@ app = FastAPI(
     version="0.1.0",
 )
 app.mount('/static', StaticFiles(directory='../swagger_ui_dep/static'), name='static')
-logging.info(f'Worker Thread: {threading.current_thread().ident:6}     tables {tb.TABLES}.')
+logger.info(f'Worker Thread: {threading.current_thread().ident:6}     tables {tb.TABLES}.')
 
 
 @app.exception_handler(StarletteHTTPException)
@@ -68,3 +110,23 @@ async def custom_app_exception_handler(request, e):
 app.include_router(img_outline.router)
 app.include_router(reqhistory.router)
 
+
+def run():
+    """Test app.main:app"""
+    img_deal_process = Process(target=img_deal, args=(img_queue,))
+    img_deal_process.start()
+    logger.info(f'********************  XBCX AI services  ********************')
+    # logging.info(f'Task tables were created by import statement {tb.TABLES}.')
+    # logging.info(f'AI micro service starting at {ct.SCHEDULE_HOST}: {ct.SCHEDULE_PORT}')
+    uvicorn.run('app.main:app',  # noqa 标准用法
+                host='0.0.0.0',
+                port=29081,
+                # ssl_keyfile=ct.SCHEDULE_KEY,
+                # ssl_certfile=ct.SCHEDULE_CER,
+                # log_level='info',
+                # workers=3
+                )
+
+
+if __name__ == '__main__':
+    run()
